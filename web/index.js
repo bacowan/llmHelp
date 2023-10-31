53a162255da1c0448aaa65efa1a969a8cecff32d
bacowan <bacowan@ualberta.ca>
2023-10-31 19:40:58 +0900

    everything works, just need to hook into framework

--- web/index.js
+++ web/index.js
@@ -1,11 +1,13 @@
-function sendClicked() {
+async function sendClicked() {
     messageBox = document.getElementById("message-box");
-    addMessage(messageBox.value, "user");
+    const userText = messageBox.value;
+    addMessage(userText, "user");
     messageBox.value = "";
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    await sendNewPrompt(userText);
 }
 
-function addMessage(text, role) { 
+function addMessage(text, role) {
     chatContainer = document.getElementById("chat-container");
 
     userMessageBubble = document.createElement("div");
@@ -17,6 +19,37 @@ function addMessage(text, role) {
     userMessageBubble.appendChild(userMessageTextP);
 
     chatContainer.appendChild(userMessageBubble);
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+async function sendNewPrompt(text) {
+    chatContainer = document.getElementById("chat-container");
+    loadingDiv = createLoadingDots();
+    chatContainer.appendChild(loadingDiv);
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    await new Promise(r => setTimeout(r, 2000));
+    const response = "hi!";
+
+    loadingDiv.remove();
+    addMessage(response, "bot");
+}
+
+function createLoadingDots() {
+    const container = document.createElement("div");
+    container.classList.add("loading-indicator");
+    container.appendChild(createDot(1));
+    container.appendChild(createDot(2));
+    container.appendChild(createDot(3));
+    return container;
+}
+
+function createDot(index) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    dot.classList.add("dot" + index);
+    return dot;
 }
 
 document.getElementById("send-button").addEventListener("click", sendClicked);
