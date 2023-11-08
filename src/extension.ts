4bd4d282e9930df350b2293ab18305a9d9491bb6
bacowan <bacowan@ualberta.ca>
2023-11-08 18:54:57 +0900

    adding logging

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,15 @@
 import * as vscode from 'vscode';
 import Framework from './framework';
 
+const winston = require('winston');
+var path = require('path');
 const fs = require('fs').promises;
 const framework : Framework = new Framework();
+
+const problemsPath = 'config/problems.json';
+const configPath = 'config/config.json';
+const chatGptModel = 'gpt-3.5-turbo';
+let logger : any;
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
@@ -19,6 +26,8 @@ export function activate(context: vscode.ExtensionContext) {
 				}
 			);
 
+			initializeLogging(panel, context);
+
 			const editor = vscode.window.activeTextEditor;
 
 			if (editor !== null && editor !== undefined) {
@@ -32,6 +41,21 @@ export function activate(context: vscode.ExtensionContext) {
 	);
 }
 
+async function initializeLogging(panel: vscode.WebviewPanel, context: vscode.ExtensionContext) {
+	const fullConfigPath = vscode.Uri.joinPath(context.extensionUri, configPath);
+	const pathUri = panel.webview.asWebviewUri(fullConfigPath);
+	const config = await fs.readFile(pathUri.fsPath, 'utf8');
+	const configAsJson = JSON.parse(config);
+	const logFolderPath = configAsJson.log_folder_path as string;
+	logger = winston.createLogger({
+		level: 'info',
+		transports: [
+			new winston.transports.File({ filename: path.join(logFolderPath, 'logs.log')})
+		]
+	});
+	framework.setLogger(logger);
+	logger.info("initialized");
+}
 			
 function loadExternalFile(oldFileName: string, html: string, context: vscode.ExtensionContext, panel: vscode.WebviewPanel): string {
 	const onDiskPath = vscode.Uri.joinPath(context.extensionUri, 'web', oldFileName);
@@ -41,7 +65,7 @@ function loadExternalFile(oldFileName: string, html: string, context: vscode.Ext
 async function loadHtml(context: vscode.ExtensionContext, panel: vscode.WebviewPanel) {
 	const onDiskPath = vscode.Uri.joinPath(context.extensionUri, 'web', 'index.html');
 	const pathUri = panel.webview.asWebviewUri(onDiskPath);
-	let html = await fs.readFile(pathUri.fsPath,'utf8');
+	let html = await fs.readFile(pathUri.fsPath, 'utf8');
 	html = loadExternalFile("index.js", html, context, panel);
 	html = loadExternalFile("styles.css", html, context, panel);
 	panel.webview.html = html;
@@ -49,7 +73,7 @@ async function loadHtml(context: vscode.ExtensionContext, panel: vscode.WebviewP
 
 async function initializeBot(context: vscode.ExtensionContext, panel: vscode.WebviewPanel) {
 	try {
-		const onDiskPath = vscode.Uri.joinPath(context.extensionUri, 'config/config.json');
+		const onDiskPath = vscode.Uri.joinPath(context.extensionUri, problemsPath);
 		const pathUri = panel.webview.asWebviewUri(onDiskPath);
 		const data = await fs.readFile(pathUri.fsPath, 'utf8');
 		panel.webview.postMessage({ command: "loadConfig", data: data });
@@ -67,12 +91,17 @@ function initializeHandlers(context: vscode.ExtensionContext, panel: vscode.Webv
 				return;
 			case 'initialize':
 				const code = editor.document.getText();
-				await framework.initialize(message.data, code);
-				const initializeResponse = await framework.sendPrompt(message.data);
-				panel.webview.postMessage({ command: "return", data: initializeResponse });
+				framework.initialize(chatGptModel, message.data, code);
+				panel.webview.postMessage({ command: "return" });
 				return;
 			case 'prompt':
-				const response = await framework.sendPrompt(message.data);
+				let response : string;
+				try {
+					response = await framework.sendPrompt(message.data);
+				}
+				catch (e) {
+					response = (e as Error).message;
+				}
 				panel.webview.postMessage({ command: "return", data: response });
 				return;
 			}
--- src/framework.ts
+++ src/framework.ts
@@ -1,34 +1,90 @@
+import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+
+const openai = new OpenAI();
+
 export default class Framework {
-    _conversationHistory : string[] = [];
-    _isInitialized = false;
-    _problem = "";
-    _code = "";
+    #logger : any;
+    #conversationHistory : ChatCompletionMessageParam[] = [];
+    #isInitialized = false;
+    #chatGptModel = "";
+    #problem = "";
+    #code = "";
 
-    get isInitialized() { return this._isInitialized; }
+    get isInitialized() { return this.#isInitialized; }
 
-    async initialize(problem: string, code: string) {
-        this._isInitialized = true;
-        this._problem = problem;
-        this._code = code;
-        this._conversationHistory = [];
+    setLogger(logger : any) {
+        this.#logger = logger;
+    }
+
+    initialize(chatGptModel: string, problem: string, code: string) {
+        this.#chatGptModel = chatGptModel;
+        this.#isInitialized = true;
+        this.#problem = problem;
+        this.#code = code;
+        this.#conversationHistory = [];
     }
 
     async sendPrompt(prompt: string) {
-        if (this._conversationHistory.length === 0) {
-            return await this.sendInitialPrompt(prompt);
+        if (this.#conversationHistory.length === 0) {
+            return await this.#sendInitialPrompt(prompt);
         }
         else {
-            return await this.sendNewPrompt(prompt);
+            return await this.#sendNewPrompt(prompt);
         }
     }
 
-    async sendInitialPrompt(prompt: string) : Promise<string> {
-        await new Promise(r => setTimeout(r, 2000));
-        return "initial prompt sent";
+    async #sendInitialPrompt(question: string) : Promise<string> {
+        const prompt = "I have been given the following instructions:\n"
+            + this.#problem + "\n\n"
+            + "I have written the following code:\n"
+            + this.#code + "\n\n"
+            + question + "\n"
+            + "I would like you to act as a teacher: "
+            + "ask me a question about why I have implemented "
+            + "the code this way in order for me to come to the conclusion myself. "
+            + "After that, ask me another question, and so on.";
+        
+        return await this.#chat(prompt);
     }
 
-    async sendNewPrompt(prompt: string) : Promise<string> {
-        await new Promise(r => setTimeout(r, 2000));
-        return "new prompt sent";
+    async #sendNewPrompt(prompt: string) : Promise<string> {
+        const isValid = await this.#validatePrompt(prompt);
+        if (isValid) {
+            const fullPrompt = prompt + "\n"
+                + "Please keep helping me, and remember to act as a teacher: don't give me any explicit answers or code.";
+            return this.#chat(fullPrompt);
+        }
+        else {
+            return this.#chat("Can you please rephrase?");
+        }
+    }
+
+    async #validatePrompt(prompt: string) : Promise<boolean> {
+        const response = await this.#chat("Please categorize the following as it relates to what you just posted: "
+                                + "\"" + prompt + "\""
+                                + ". Is it: relevant, irrelevant, or relevant but incorrect? Please give a one word response.",
+                                false);
+        return !response.toLowerCase().includes("irrelevant");
+    }
+
+    async #chat(prompt: string, includeInHistory: boolean = true): Promise<string> {
+        this.#conversationHistory.push({role: "user", content: prompt});
+        const response = await openai.chat.completions.create({
+            messages: this.#conversationHistory,
+            model: this.#chatGptModel
+        });
+        const responseText = response['choices'][0]['message']['content'] ?? "";
+
+        this.#logger.info("Prompt: " + prompt);
+        this.#logger.info("Response: " + responseText);
+
+        if (includeInHistory) {
+            this.#conversationHistory.push({role: "assistant", content: responseText});
+        }
+        else {
+            this.#conversationHistory.pop();
+        }
+        return responseText;
     }
 }
